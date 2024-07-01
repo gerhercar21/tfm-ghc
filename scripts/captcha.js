891d2c4e581028221e05697051fdf8020df3aa58
gerhercar21 <gerardohdezcarrera@gmail.com>
2024-07-01 13:13:47 -0600

    Formulario finish

--- scripts/captcha.js
+++ scripts/captcha.js
@@ -1,19 +1,19 @@
     function validateForm() {
         var response = grecaptcha.getResponse();
         if (response.length === 0) {
-            document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">Por favor completa el reCAPTCHA.</span>';
+            document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">Verifica que no eres un robot...</span>';
             return false;
         } else {
-            document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:green;">¡reCAPTCHA completado!</span>';
+            document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:green;">¡Eres un humano!</span>';
             return true;
         }
     }
 
     function verifyCaptcha(response) {
         if (response === '') {
-            document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">Por favor completa el reCAPTCHA.</span>';
+            document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">Verifica que no eres un robot...</span>';
         } else {
-            document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:green;">¡reCAPTCHA completado!</span>';
+            document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:green;">¡Eres un humano!</span>';
         }
     }
 
