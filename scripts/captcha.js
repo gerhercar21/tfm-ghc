ccc89473e64d337be6f4df9dff961eec5e7f3876
gerhercar21 <gerardohdezcarrera@gmail.com>
2024-07-01 14:00:12 -0600

    last

--- scripts/captcha.js
+++ scripts/captcha.js
@@ -1,9 +1,4 @@
     function validateForm() {
-        // Verificar el ancho de la pantalla
-        if (window.innerWidth <= 575) {
-            return true; // No se aplica la validación en pantallas pequeñas
-        }
-        
         var response = grecaptcha.getResponse();
         if (response.length === 0) {
             document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">Verifica que no eres un robot...</span>';
@@ -15,14 +10,10 @@
     }
 
     function verifyCaptcha(response) {
-        // Verificar el ancho de la pantalla
-        if (window.innerWidth <= 575) {
-            return; // No se aplica la validación en pantallas pequeñas
-        }
-
         if (response === '') {
             document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">Verifica que no eres un robot...</span>';
         } else {
             document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:green;">¡Eres un humano!</span>';
         }
     }
+
